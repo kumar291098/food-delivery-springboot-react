60423b90e300bdc80ae5668fffa034b544edab7c
Avnish <kumara291098@gmail.com>
2026-03-12 02:36:18 +0530

    user service design

---
+++ user-service/src/main/java/com/foodapp/userservice/dto/LoginResponse.java
@@ -0,0 +1,28 @@
+package com.foodapp.userservice.dto;
+
+public class LoginResponse {
+
+	private String message;
+	private UserResponse user;
+
+	public LoginResponse(String message, UserResponse user) {
+		this.message = message;
+		this.user = user;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public UserResponse getUser() {
+		return user;
+	}
+
+	public void setUser(UserResponse user) {
+		this.user = user;
+	}
+}
